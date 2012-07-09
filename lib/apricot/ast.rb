8fba9c4bb96fc76448935816dc3d480179f7323c
Scott Olson <scott@scott-olson.org>
2012-07-08 21:22:10 -0600

    Revert "Save one indent level. [ci skip]"
    This reverts commit 64a247ec62609de47e1963d2d269c69e1664ed09.

--- lib/apricot/ast.rb
+++ lib/apricot/ast.rb
@@ -1,56 +1,58 @@
-module Apricot::AST
-  class Node
-    attr_reader :line
+module Apricot
+  module AST
+    class Node
+      attr_reader :line
 
-    def initialize(line)
-      @line = line
+      def initialize(line)
+        @line = line
+      end
+
+      def pos(g)
+        g.set_line(@line)
+      end
     end
 
-    def pos(g)
-      g.set_line(@line)
+    class SimpleNode < Node
+      attr_reader :value
+
+      def initialize(line, value)
+        super(line)
+        @value = value
+      end
     end
-  end
 
-  class SimpleNode < Node
-    attr_reader :value
-
-    def initialize(line, value)
-      super(line)
-      @value = value
+    class List < SimpleNode
     end
-  end
 
-  class List < SimpleNode
-  end
+    class Array < SimpleNode
+    end
 
-  class Array < SimpleNode
-  end
+    class Hash < SimpleNode
+    end
 
-  class Hash < SimpleNode
-  end
+    class String < SimpleNode
+    end
 
-  class String < SimpleNode
-  end
+    class Identifier < SimpleNode
+    end
 
-  class Identifier < SimpleNode
-  end
+    class Symbol < SimpleNode
+    end
 
-  class Symbol < SimpleNode
-  end
+    class Integer < SimpleNode
+    end
 
-  class Integer < SimpleNode
-  end
+    class Float < SimpleNode
+    end
 
-  class Float < SimpleNode
-  end
+    class Rational < Node
+      attr_reader :numerator, :denominator
 
-  class Rational < Node
-    attr_reader :numerator, :denominator
-
-    def initialize(line, numerator, denominator)
-      super(line)
-      @numerator = numerator
-      @denominator = denominator
+      def initialize(line, numerator, denominator)
+        super(line)
+        @numerator = numerator
+        @denominator = denominator
+      end
     end
   end
 end
