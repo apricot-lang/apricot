0d249e4e3e18f27ed9425f316aed52d4211ac8fe
Curtis McEnroe <programble@gmail.com>
2012-07-11 23:40:05 -0400

    Add if special form

--- lib/apricot/special_forms.rb
+++ lib/apricot/special_forms.rb
@@ -39,4 +39,27 @@ module Apricot
     g.local_count = 1
     g.local_names = [name]
   end
+
+  SpecialForm.define(:if) do |g, args|
+    raise ArgumentError, "Too few arguments to if" if args.length < 2
+    raise ArgumentError, "Too many arguments to if" if args.length > 3
+
+    cond, body, else_body = args
+    else_label, end_label = g.new_label, g.new_label
+
+    cond.bytecode(g)
+    g.gif else_label
+
+    body.bytecode(g)
+    g.goto end_label
+
+    else_label.set!
+    if else_body
+      else_body.bytecode(g)
+    else
+      g.push_nil
+    end
+
+    end_label.set!
+  end
 end
