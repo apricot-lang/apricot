fedbbbb3e40e8e5d34d0213cfcab438e4aae89fe
Curtis McEnroe <programble@gmail.com>
2012-07-12 00:27:22 -0400

    Add do special form

--- lib/apricot/special_forms.rb
+++ lib/apricot/special_forms.rb
@@ -62,4 +62,16 @@ module Apricot
 
     end_label.set!
   end
+
+  SpecialForm.define(:do) do |g, args|
+    if args.empty?
+      g.push_nil
+    else
+      args[0..-2].each do |a|
+        a.bytecode(g)
+        g.pop
+      end
+      args.last.bytecode(g)
+    end
+  end
 end
