5674e87bbfcd641c6d0f8ad5cefe29a14d422400
Scott Olson <scott@scott-olson.org>
2012-07-27 01:13:05 -0600

    Add (.method receiver args*) send expression form

--- lib/apricot/ast/list.rb
+++ lib/apricot/ast/list.rb
@@ -16,13 +16,30 @@ module Apricot::AST
         callee = @elements.shift
         args = @elements
 
-        if callee.is_a?(Identifier) && special = Apricot::SpecialForm[callee.name]
-          special.bytecode(g, args)
-        else
-          callee.bytecode(g)
-          args.each {|arg| arg.bytecode(g) }
-          g.send :call, args.length
+        if callee.is_a?(Identifier)
+          name = callee.name
+
+          # Handle special forms such as def, let, fn, quote, etc
+          if special = Apricot::SpecialForm[name]
+            special.bytecode(g, args)
+            return
+
+          # Handle the (.method receiver args*) send expression form
+          elsif name.to_s.start_with?('.') && name.to_s != '..'
+            raise ArgumentError, "Too few arguments to send expression, expecting (.method receiver ...)" if args.empty?
+
+            method = Identifier.new(callee.line, callee.name[1..-1].to_sym)
+            args.insert(1, method)
+
+            Apricot::SpecialForm[:'.'].bytecode(g, args)
+            return
+          end
         end
+
+        # TODO: macros
+        callee.bytecode(g)
+        args.each {|arg| arg.bytecode(g) }
+        g.send :call, args.length
       end
     end
 
