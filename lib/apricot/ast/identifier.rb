7b4ea77c71662f161ec2fec8c4339092d5e4a080
Scott Olson <scott@scott-olson.org>
2013-01-10 12:19:43 -0600

    Remove duplicated code by putting identifiers in AST identifiers

--- lib/apricot/ast/identifier.rb
+++ lib/apricot/ast/identifier.rb
@@ -1,16 +1,9 @@
 module Apricot
   module AST
     class Identifier < Node
-      attr_reader :name
-
       def initialize(line, name)
         super(line)
-        @name = name
-
-        if @name =~ /\A(?:[A-Z]\w*::)*[A-Z]\w*\z/
-          @constant = true
-          @const_names = @name.to_s.split('::').map(&:to_sym)
-        end
+        @id = Apricot::Identifier.intern(name)
       end
 
       def reference(g)
@@ -21,13 +14,16 @@ module Apricot
                        end
       end
 
+      def name
+        @id.name
+      end
+
       def constant?
-        @constant
+        @id.constant?
       end
 
       def const_names
-        raise "#{@name} is not a constant" unless constant?
-        @const_names
+        @id.const_names
       end
 
       def bytecode(g)
@@ -35,7 +31,7 @@ module Apricot
 
         if constant?
           g.push_cpath_top
-          @const_names.each {|n| g.find_const n }
+          const_names.each {|n| g.find_const n }
         else
           reference(g).bytecode(g)
         end
@@ -45,17 +41,17 @@ module Apricot
       def assign_bytecode(g, value)
         if constant?
           g.push_cpath_top
-          @const_names[0..-2].each {|n| g.find_const n }
-          g.push_literal @const_names.last
+          const_names[0..-2].each {|n| g.find_const n }
+          g.push_literal const_names.last
           value.bytecode(g)
           g.send :const_set, 2
         else
-          g.compile_error "Can't change the value of self" if @name == :self
+          g.compile_error "Can't change the value of self" if name == :self
 
           g.push_cpath_top
           g.find_const :Apricot
           g.send :current_namespace, 0
-          g.push_literal @name
+          g.push_literal name
           value.bytecode(g)
           g.send :set_var, 2
         end
@@ -67,17 +63,17 @@ module Apricot
         g.push_cpath_top
         g.find_const :Apricot
         g.find_const :Identifier
-        g.push_literal @name
+        g.push_literal name
         g.send :intern, 1
       end
 
       def namespace_fn?(g)
         reference(g).is_a?(NamespaceReference) &&
-          Apricot.current_namespace.fns.include?(@name)
+          Apricot.current_namespace.fns.include?(name)
       end
 
       def to_value
-        Apricot::Identifier.intern @name
+        @id
       end
 
       def node_equal?(other)
