6b4d1207290b619b07a05c1c9be9ebfe1b30a4be
Scott Olson <scott@scott-olson.org>
2013-04-14 04:31:54 -0600

    *Massively* simplify compiler machinery. Goodbye, stages!

--- lib/apricot/compiler.rb
+++ lib/apricot/compiler.rb
@@ -1,44 +1,45 @@
 module Apricot
-  class Compiler < Rubinius::Compiler
-    def self.compile(file, output_file = nil)
-      output_file ||= Rubinius::Compiler.compiled_name(file)
+  module Compiler
+    module_function
 
-      compiler = new :apricot_file, :compiled_file
-      compiler.parser.input file
-      compiler.writer.name = output_file
-      prepare_compiled_code(compiler.run)
-    end
-
-    def self.compile_string(code, file = "(none)", line = 1)
-      compiler = new :apricot_string, :compiled_method
-      compiler.parser.input(code, file, line)
-      prepare_compiled_code(compiler.run)
-    end
-
-    def self.compile_node(node, file = "(none)", line = 1)
-      compiler = new :apricot_bytecode, :compiled_method
-      compiler.generator.input AST::TopLevel.new([node], file, line, false)
-      prepare_compiled_code(compiler.run)
-    end
-
-    def self.compile_form(form, file = "(none)", line = 1)
-      compile_node(AST::Node.from_value(form, line), file, line)
-    end
-
-    def self.prepare_compiled_code(cc)
+    def generate(node, file = "(none)", line = 1)
+      gen = Apricot::Generator.new
+      node.bytecode(gen)
+      gen.close
+      gen.encode
+      cc = gen.package(Rubinius::CompiledCode)
       cc.scope = Rubinius::ConstantScope.new(Object)
       cc
     end
 
-    def self.eval(code, file = "(eval)", line = 1)
+    def compile(file)
+      ast = Apricot::Parser.parse_file(file)
+      generate(ast, file)
+    end
+
+    def compile_string(code, file = "(none)", line = 1)
+      ast = Apricot::Parser.parse_string(code, file, line)
+      generate(ast, file, line)
+    end
+
+    def compile_node(node, file = "(none)", line = 1)
+      ast = AST::TopLevel.new([node], file, line, false)
+      generate(ast, file, line)
+    end
+
+    def compile_form(form, file = "(none)", line = 1)
+      compile_node(AST::Node.from_value(form, line), file, line)
+    end
+
+    def eval(code, file = "(eval)", line = 1)
       Rubinius.run_script(compile_string(code, file, line))
     end
 
-    def self.eval_form(form, file = "(eval)", line = 1)
+    def eval_form(form, file = "(eval)", line = 1)
       Rubinius.run_script(compile_form(form, file, line))
     end
 
-    def self.eval_node(node, file = "(eval)", line = 1)
+    def eval_node(node, file = "(eval)", line = 1)
       Rubinius.run_script(compile_node(node, file, line))
     end
   end
