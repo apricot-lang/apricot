cbdd12213120726f8bdd9506709a24dc79df6cae
Scott Olson <scott@scott-olson.org>
2013-04-20 17:20:01 -0600

    Add beginning of Seq bytecode.

--- lib/apricot/compiler.rb
+++ lib/apricot/compiler.rb
@@ -111,7 +111,7 @@ module Apricot
 
     SELF = Identifier.intern(:self)
 
-    def bytecode(g, form, quoted = false)
+    def bytecode(g, form, quoted = false, macroexpand = true)
       pos(g, form)
 
       case form
@@ -136,7 +136,75 @@ module Apricot
         end
 
       when Seq
-        raise NotImplementedError, "seq bytecode"
+        if quoted || form.empty?
+          g.push_const :Apricot
+          g.find_const :List
+
+          if form.empty?
+            g.find_const :EMPTY_LIST
+          else
+            form.each {|e| bytecode(g, e, true) }
+            g.send :[], form.count
+          end
+        else
+          callee, args = form.first, form.rest
+
+          # Handle special forms such as def, let, fn, quote, etc
+          if callee.is_a?(Identifier) && special = SpecialForm[callee.name]
+            special.bytecode(g, args)
+            return
+          end
+
+          if macroexpand
+            form = Apricot.macroexpand(form)
+
+            if form.is_a?(List)
+              # Avoid recursing and macroexpanding again if expansion returns a list
+              bytecode(g, form, false, false)
+            else
+              bytecode(g, form)
+            end
+
+            return
+          end
+
+          # Handle (foo ...) and (Foo/bar ...) calls
+          if callee.is_a?(Identifier)
+            # meta = callee.meta(g)
+
+            # # Handle inlinable function calls
+            # if meta && meta[:inline] && (!meta[:'inline-arities'] ||
+            #                              meta[:'inline-arities'].apricot_call(args.length))
+            #   # Apply the inliner macro to the arguments and compile the result.
+            #   inliner = meta[:inline]
+            #   args.map!(&:to_value)
+
+            #   begin
+            #     inlined_form = inliner.apricot_call(*args)
+            #   rescue => e
+            #     g.compile_error "Inliner macro for '#{callee.name}' raised an exception:\n  #{e}"
+            #   end
+
+            #   Node.from_value(inlined_form, line).bytecode(g)
+            #   return
+            if callee.fn? || callee.method?
+              ns_id = Apricot::Identifier.intern(callee.ns.name)
+              first_name, *rest_names = ns_id.const_names
+
+              g.push_const first_name
+              rest_names.each {|n| g.find_const(n) }
+
+              args.each {|arg| bytecode(g, arg) }
+              g.send callee.unqualified_name, args.count
+              return
+            end
+          end
+
+          # Handle everything else
+          bytecode(g, callee)
+          args.each {|arg| bytecode(g, arg) }
+          g.send :apricot_call, args.count
+        end
 
       when Symbol
         g.push_literal form
