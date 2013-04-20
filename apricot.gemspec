de4f812e2acb992d98bd4bb0cc5b0b6bf1455dbd
Curtis McEnroe <programble@gmail.com>
2013-04-20 16:42:58 -0400

    Use SimpleCov to generate code coverage reports

--- apricot.gemspec
+++ apricot.gemspec
@@ -20,5 +20,6 @@ Gem::Specification.new do |s|
 
   s.add_development_dependency "rake", "~> 10.0.3"
   s.add_development_dependency "rspec", "~> 2.13.0"
+  s.add_development_dependency "simplecov", "~> 0.7.0"
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,33 @@
 require 'bundler/setup'
 require 'rspec'
+
+require 'simplecov'
+
+SimpleCov.start do
+  add_filter 'spec/'
+  add_filter 'kernel/' # SimpleCov doesn't understand Apricot code
+
+  add_group 'Compiler' do |src|
+    ['code_loader.rb',
+     'compiler.rb',
+     'errors.rb',
+     'generator.rb',
+     'reader.rb'].any? {|f| src.filename.end_with? "apricot/#{f}" }
+  end
+  add_group 'AST', 'ast'
+  add_group 'Special Forms', 'special_forms'
+  add_group 'Runtime' do |src|
+    ['boot.rb',
+     'cons.rb',
+     'identifier.rb',
+     'list.rb',
+     'macroexpand.rb',
+     'misc.rb',
+     'namespace.rb',
+     'ruby_ext.rb',
+     'seq.rb'].any? {|f| src.filename.end_with? "apricot/#{f}" }
+  end
+end
+
 require 'apricot'
 include Apricot
